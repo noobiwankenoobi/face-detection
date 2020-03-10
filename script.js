bb010f5027ca2ea90eecae66cecbad0612fc4879
Ross Hazelett <rosshazelett@Rosss-MacBook-Pro-2.local>
2020-03-10 19:07:24 -0400

    trying to load in the models

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-
 const video = document.getElementById('myVideo');
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(startVideo)
+
+console.log(faceapi.nets)
+
 const startVideo = () => {
-    // gets two params-- empty object video and what's coming from webcam (stream)
+    // promise -- gets two params-- empty object video and what's coming from webcam (stream)
     navigator.getUserMedia(
         { video: {} }, 
         stream => video.srcObject = stream,
