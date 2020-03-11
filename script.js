93e98bc2287a797429de362f63603eee461cfbca
Ross Hazelett <rosshazelett@Rosss-MacBook-Pro-2.local>
2020-03-10 21:03:42 -0400

    got faceapi.nets to load and log to console, need video event listener next

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const video = document.getElementById('myVideo');
 
+const startVideo = () => {
+    // promise -- gets two params-- empty object video and what's coming from webcam (stream)
+    navigator.getUserMedia( { video: {} }, stream => video.srcObject = stream, err => console.error(err) )
+};
+
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/models')
   ]).then(startVideo)
 
-console.log(faceapi.nets)
+console.log("faceapi.nets= ", faceapi.nets)
 
-const startVideo = () => {
-    // promise -- gets two params-- empty object video and what's coming from webcam (stream)
-    navigator.getUserMedia(
-        { video: {} }, 
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
-};
 
-startVideo();
+
+// startVideo();
