06bcb82aae9b237d17fe58cde2c0b56c28b17201
Ross Hazelett <rosshazelett@Rosss-MacBook-Pro-2.local>
2020-03-11 13:33:49 -0400

    everything working at the moment

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const video = document.getElementById('myVideo');
 
+
+
 const startVideo = () => {
     // promise -- gets two params-- empty object video and what's coming from webcam (stream)
     navigator.getUserMedia( { video: {} }, stream => video.srcObject = stream, err => console.error(err) )
 };
+
+// const startVideo2 = () => {
+//     navigator.mediaDevices.getUserMedia({video: {})
+//         .then(function(stream) {
+//   /* use the stream */
+//         })
+//         .catch(function(err) {
+//   /* handle the error */
+//         });
+// }
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -12,8 +24,25 @@ Promise.all([
     faceapi.nets.faceExpressionNet.loadFromUri('/models')
   ]).then(startVideo)
 
+
+
 console.log("faceapi.nets= ", faceapi.nets)
 
 
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displayDimensions = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displayDimensions)
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+      const resizedDetections = faceapi.resizeResults(detections, displayDimensions)
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    //   console.log("detections = ", detections)
+    }, 100)
+  })
 
 // startVideo();
